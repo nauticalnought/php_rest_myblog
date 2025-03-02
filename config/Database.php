4f7c6f8b9c005ed828f70d60f0252a60b35ed535
Daniel Sones <djsones@mail.fhsu.edu>
2025-03-01 21:03:12 -0800

    replaced host, db name, etc with getenv

--- config/Database.php
+++ config/Database.php
@@ -1,24 +1,31 @@
 <?php 
-  class Database {
-    // DB Params
-    private $host = 'localhost';
-    private $db_name = 'myblog';
-    private $username = 'root';
-    private $password = '';
-    private $conn;
+class Database {
+  // DB Params (use environment variables)
+  private $host = '';
+  private $db_name = '';
+  private $username = '';
+  private $password = '';
+  private $conn;
 
-    // DB Connect
-    public function connect() {
-      $this->conn = null;
+  // DB Connect
+  public function connect() {
+    $this->conn = null;
 
-      try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, 
-        $this->username, $this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      } catch(PDOException $e) {
-        echo 'Connection Error: ' . $e->getMessage();
-      }
+    // Get environment variables
+    $this->host = getenv('DB_HOST');  // Set DB host from environment variable
+    $this->db_name = getenv('DB_NAME');  // Set DB name from environment variable
+    $this->username = getenv('DB_USER');  // Set DB user from environment variable
+    $this->password = getenv('DB_PASS');  // Set DB password from environment variable
 
-      return $this->conn;
+    try { 
+      // Create PDO connection string
+      $dsn = 'pgsql:host=' . $this->host . ';dbname=' . $this->db_name;
+      $this->conn = new PDO($dsn, $this->username, $this->password);
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+      echo 'Connection Error: ' . $e->getMessage();
     }
+
+    return $this->conn;
   }
+}
