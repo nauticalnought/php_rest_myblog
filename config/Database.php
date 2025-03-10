c9510509b2119e64b993282f137cd17bacdcd9ac
Daniel Sones <djsones@mail.fhsu.edu>
2025-03-10 12:29:26 -0700

    .

--- config/Database.php
+++ config/Database.php
@@ -1,23 +1,36 @@
 <?php 
   class Database {
     // DB Params
-    private $host = 'localhost';
-    private $db_name = 'myblog';
-    private $username = 'root';
-    private $password = '';
     private $conn;
+    private $host;
+    private $port;
+    private $db_name;
+    private $username;
+    private $password;
+    
+    public function __construct(){
+      $this->username = getenv('USERNAME');
+      $this->password = getenv('PASSWORD');
+      $this->dbname = getenv('DBNAME');
+      $this->host = getenv('HOST');
+      $this->port = getenv('PORT');
+    }
 
     // DB Connect
     public function connect() {
-      $this->conn = null;
+      if ($this->conn) {
+        return $this->conn;
+      } else {
 
-      try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      } catch(PDOException $e) {
-        echo 'Connection Error: ' . $e->getMessage();
+        $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
+
+        try { 
+          $this->conn = new PDO($dsn, $this->username, $this->password);
+          $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          return $this->conn;
+        } catch(PDOException $e) {
+          echo 'Connection Error: ' . $e->getMessage();
+        }  
       }
-
-      return $this->conn;
     }
   }
